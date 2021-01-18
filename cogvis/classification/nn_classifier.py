5c8c0c69e8d316a68df6b98f59631c11607461d3
TimothySimons <smntim001@myuct.ac.za>
2021-01-18 16:54:04 +0200

    Implement load functionality in base class

--- cogvis/classification/nn_classifier.py
+++ cogvis/classification/nn_classifier.py
@@ -1,20 +1,30 @@
+import multiprocessing
+
+import numpy as np
 import torch
-from torchvision import transforms
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
 
 class NeuralNetClassifier:
+
     def __init__(self):
         pass
 
-    def transform(self, mean, std, **transform_params):
-        # TODO: doc - must pass in the class of transform as the key and the 
-        # params for that class as the value.
+    def load(self, data_dir, batch_size, **transform_params):
+        data_transform = self._transform(transform_params)
+        dataset = datasets.ImageFolder(root=data_dir, transform=data_transform)
+        num_procs = multiprocessing.cpu_count() - 1
+        #inputs, classes = next(iter(data_loader))
+        #print(inputs, classes)
+        return DataLoader(dataset, batch_size, shuffle=True, num_workers=num_procs)
+
+    def _transform(self, transform_params):
         data_transforms = []
         for k, v in transform_params.items():
-            expr = f"transforms.{k}({v if v is not None else ''})"
+            expr = f"transforms.{k}{v}"
             data_transforms.append(eval(expr))
-        data_transforms.append(transforms.ToTensor())
-        data_transforms.append(transforms.Normalize(mean, std))
-        data_transforms = transforms.Compose(data_transforms)
+        return transforms.Compose(data_transforms)
 
 
     def train(self, X, y):
@@ -30,6 +40,19 @@ class MLP(NeuralNetClassifier):
 
 
 if __name__== '__main__':
+    # Just ignore this - random testing
+    from os import listdir
+    from os.path import isfile, join
+    from PIL import Image
+    class_dir = 'hymenoptera_data/train'
+    #image_data = []
+    #for filename in listdir(data_dir):
+    #    filename = join(data_dir, filename)
+    #    image = Image.open(filename)
+    #    image_data.append(np.asarray(image))
+    #print(image_data[0].shape)
+    #print(image_data[1].shape)
     model = MLP()
-    model.transform(0.5, 0.5, RandomResizedCrop=224)
+    model.load(class_dir, 4, RandomResizedCrop=(224,), ToTensor=(), Normalize = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
+
 
