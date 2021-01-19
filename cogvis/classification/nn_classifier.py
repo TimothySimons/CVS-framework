9b25cb342beb068b58029a47a33167049c286057
TimothySimons <smntim001@myuct.ac.za>
2021-01-19 12:29:44 +0200

    Finish first iteration of MLP model

--- cogvis/classification/nn_classifier.py
+++ cogvis/classification/nn_classifier.py
@@ -2,22 +2,22 @@ import multiprocessing
 
 import numpy as np
 import torch
+import torch.nn as nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 
-class NeuralNetClassifier:
+class NeuralNetClassifier(nn.Module):
 
     def __init__(self):
         pass
 
-    def load(self, data_dir, batch_size, **transform_params):
+    def get_data_loader(self, data_dir, batch_size, **transform_params):
         data_transform = self._transform(transform_params)
         dataset = datasets.ImageFolder(root=data_dir, transform=data_transform)
         num_procs = multiprocessing.cpu_count() - 1
-        #inputs, classes = next(iter(data_loader))
-        #print(inputs, classes)
-        return DataLoader(dataset, batch_size, shuffle=True, num_workers=num_procs)
+        return DataLoader(dataset, batch_size, shuffle=True, 
+                num_workers=num_procs)
 
     def _transform(self, transform_params):
         data_transforms = []
@@ -35,8 +35,23 @@ class NeuralNetClassifier:
 
 
 class MLP(NeuralNetClassifier):
-    def __init__(self):
+    def __init__(self, input_size, output_size, hidden_sizes):
         super().__init__()
+        assert hidden_sizes, "hidden_sizes is empty" 
+        self.input_fc = nn.Linear(input_size, hidden_sizes[0])
+        self.hidden_fc = nn.ModuleList()
+        for k in range(len(hidden_sizes)-1):
+            self.hidden_fc.append(nn.Linear(hidden_sizes[k], hidden_sizes[k+1]))
+        self.output_fc = nn.Linear(input_size, hidden_sizes[-1])
+
+    def forward(self):
+        x = F.relu(self.input_fc)
+        for l in hidden_fc:
+            x = F.relu(l)
+        x = F.relu(self.output_fc)
+        return x
+
+
 
 
 if __name__== '__main__':
