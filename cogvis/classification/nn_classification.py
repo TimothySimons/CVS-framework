b9cb9221dfc99437dc22917530dde4a464160d01
TimothySimons <smntim001@myuct.ac.za>
2021-01-25 14:22:05 +0200

    Refactor train function

--- cogvis/classification/nn_classification.py
+++ cogvis/classification/nn_classification.py
@@ -32,7 +32,6 @@ class MLP(nn.Module):
 
 
 class CNN(nn.Module):
-    # TODO: allow them to choose more parameters, like stride etc.
 
     def __init__(self, conv_dims, linear_dims, conv_ks, pool_ks):
         super(CNN, self).__init__()
@@ -138,3 +137,107 @@ def train(model, train_loader, val_loader, train_size, val_size, criterion,
     if device == 'gpu':
         model = model.to(torch.device('cpu'))
     return model
+
+
+def new_train(model, train_loader, val_loader, train_size, val_size, criterion, 
+        optimizer, scheduler, num_epochs, device='cpu'):
+    device = torch.device('cuda:0' if device == 'gpu' else 'cpu')
+    model = model.to(device)
+    criterion = criterion.to(device)
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    for epoch in range(num_epochs):
+        print(f'Epoch: {epoch}/{num_epochs-1}')
+        print('-' * 10)
+        epoch_loss, epoch_acc = epoch_train(model, train_loader, train_size, 
+                criterion, optimizer, scheduler, device)
+        print(f'Train Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+        epoch_loss, epoch_acc = epoch_eval(model, val_loader, val_size, 
+                criterion, device)
+        print(f'Val. Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+        if epoch_acc > best_acc:
+            best_acc = epoch_acc
+            best_model_wts = copy.deepcopy(model.state_dict())
+        print()
+    print(f'Best Val. Acc: {best_acc:4f}')
+    model.load_state_dict(best_model_wts)
+    if device == 'gpu':
+        model = model.to(torch.device('cpu'))
+    return model
+
+
+def epoch_train(model, loader, size, criterion, optimizer, scheduler, device):
+    model.train()  
+    running_loss = 0.0
+    running_corrects = 0
+    for inputs, labels in loader: 
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+        scheduler.step()
+    epoch_loss = running_loss / size
+    epoch_acc = running_corrects.double() / size 
+    return epoch_loss, epoch_acc
+
+
+def epoch_eval(model, loader, size, criterion, device):
+    model.eval()  
+    running_loss = 0.0
+    running_corrects = 0
+    with torch.no_grad():
+        for inputs, labels in loader: 
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+    epoch_loss = running_loss / size
+    epoch_acc = running_corrects.double() / size 
+    return epoch_loss, epoch_acc
+
+
+if __name__ == '__main__':
+    train_transform = data_transform(
+        Resize = (256,),
+        CenterCrop= (224,),
+        ToTensor = (),
+        Normalize = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    )
+
+    val_transform = data_transform(
+        Resize = (256,),
+        CenterCrop = (224,),
+        ToTensor = (), 
+        Normalize = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    )
+
+    train_dir = 'hymenoptera_data/train/'
+    val_dir = 'hymenoptera_data/val/'
+    train_size, train_loader = data_loader(train_dir, 4, train_transform)
+    val_size, val_loader = data_loader(val_dir, 4, val_transform)
+
+    #nn_model = nn_classifier.MLP([150528, 250, 2])
+    #nn_model = nn_classifier.CNN([3, 6, 16], [16 * 53 * 53, 120, 84, 2], conv_ks=5, pool_ks=2)
+    #nn_model = nn_classifier.existing_model('resnet18', 2)
+    nn_model = existing_model('resnet18', 2)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(nn_model.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+    nn_model = new_train(
+            nn_model, 
+            train_loader, val_loader, 
+            train_size, val_size, 
+            criterion, 
+            optimizer,
+            exp_lr_scheduler, 
+            num_epochs=5,
+            )
