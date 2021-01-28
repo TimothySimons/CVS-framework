c93e1b7364637f44e0d0014a3bd17e60f2e8cc41
TimothySimons <smntim001@myuct.ac.za>
2021-01-28 15:33:33 +0200

    Add support for existing PyTorch models
    The models that cogvis supports are all versions of resnet, alexnet, vgg,
    squeezenet and densenet. Inception is not supported because it has two
    outputs (auxiliary and primary) which interferes with the
    nn_classification.train function. It also requires input images of size
    (299, 299) which is kind of annoying.

--- cogvis/classification/nn_classification.py
+++ cogvis/classification/nn_classification.py
@@ -57,16 +57,34 @@ class CNN(nn.Module):
         return F.softmax(self.linears[-1](x), dim=1)
 
 
-def existing_model(model_name, out_size, *args, **kwargs):
-    args_str = ''.join(f'{a},' for a in args)
-    kwargs_str = args_str.join(f'{k}={v},' for k, v in kwargs.items())
-    model =  eval(f'models.{model_name}({kwargs_str})')
-    num_features = model.fc.in_features
-    model.fc = nn.Linear(num_features, out_size)
+def existing(model_name, num_classes, pretrained=False, feature_extract=False):
+    model =  eval(f'models.{model_name}(pretrained={pretrained})')
+    if 'resnet' in model_name:
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features, num_classes)
+    elif 'alexnet' in model_name or 'vgg' in model_name:
+        in_features = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(in_features, num_classes)
+    elif 'squeezenet' in model_name:
+        in_features = model.classifier[1].in_features
+        model.classifier[1] = nn.Conv2d(in_features, num_classes, 
+                kernel_size=(1,1), stride=(1,1))
+    elif 'densenet' in model_name:
+        in_features = model.classifier[1].in_features
+        model.classifier = nn.Linear(in_features, num_classes)
+    else:
+        raise NotImplementedError(f'{model_name} not supported')
+    _requires_grad(model, feature_extract)
     return model
 
 
-def data_loader(data_dir, batch_size, data_transform): 
+def _requires_grad(model, feature_extract):
+    if feature_extract:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def data_loader(data_dir, batch_size, data_transform=None): 
     dataset = datasets.ImageFolder(root=data_dir, transform=data_transform)
     num_procs = multiprocessing.cpu_count() - 1
     loader = DataLoader(dataset, batch_size, shuffle=True, 
@@ -83,63 +101,6 @@ def data_transform(**kwargs):
 
 
 def train(model, train_loader, val_loader, train_size, val_size, criterion, 
-        optimizer, scheduler, num_epochs, device='cpu'):
-
-    data_loaders = {'train': train_loader, 'val': val_loader}
-    dataset_sizes = {'train': train_size, 'val': val_size}
-    device = torch.device('cuda:0' if device == 'gpu' else 'cpu')
-    model = model.to(device)
-    criterion = criterion.to(device)
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-    for epoch in range(num_epochs):
-        print(f'Epoch: {epoch}/{num_epochs-1}')
-        print('-' * 10)
-
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  
-            else:
-                model.eval()  
-
-            running_loss = 0.0
-            running_corrects = 0
-            for inputs, labels in data_loaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-                optimizer.zero_grad()
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            if phase == 'train':
-                scheduler.step()
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-    print(f'Best val Acc: {best_acc:4f}')
-
-    model.load_state_dict(best_model_wts)
-    if device == 'gpu':
-        model = model.to(torch.device('cpu'))
-    return model
-
-
-def new_train(model, train_loader, val_loader, train_size, val_size, criterion, 
         optimizer, scheduler, num_epochs, device='cpu'):
     device = torch.device('cuda:0' if device == 'gpu' else 'cpu')
     model = model.to(device)
@@ -181,7 +142,7 @@ def epoch_train(model, loader, size, criterion, optimizer, scheduler, device):
         optimizer.step()
         running_loss += loss.item() * inputs.size(0)
         running_corrects += torch.sum(preds == labels.data)
-        scheduler.step()
+    scheduler.step()
     epoch_loss = running_loss / size
     epoch_acc = running_corrects.double() / size 
     return epoch_loss, epoch_acc
@@ -206,38 +167,52 @@ def epoch_eval(model, loader, size, criterion, device):
 
 
 if __name__ == '__main__':
-    train_transform = data_transform(
-        Resize = (256,),
-        CenterCrop= (224,),
-        ToTensor = (),
-        Normalize = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    )
+    train_dir = 'orchard_data/train/'
+    val_dir = 'orchard_data/val/'
+    test_dir = 'orchard_data/test/'
+    # ----- 1 ----- #
+    transform = data_transform(
+            Resize=(70,), 
+            CenterCrop=(64,),
+            ToTensor=(),
+            )
+    size, loader = data_loader(train_dir, 1000, transform)
+    data, _ = next(iter(loader))
+    means = [data[:,c].mean().tolist() for c in range(len(data[0]))] 
+    stds = [data[:,c].std().tolist() for c in range(len(data[0]))]
 
-    val_transform = data_transform(
-        Resize = (256,),
-        CenterCrop = (224,),
-        ToTensor = (), 
-        Normalize = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    )
+    # ----- 2 ----- #
+    batch_size = 4
+    transform = data_transform(
+        Resize=(70,),
+        CenterCrop=(64,),
+        ToTensor=(),
+        Normalize=(means, stds),
+        )
+    train_size, train_loader = data_loader(train_dir, batch_size, transform)
+    print(train_size)
+    val_size, val_loader = data_loader(val_dir, batch_size, transform)
 
-    train_dir = 'hymenoptera_data/train/'
-    val_dir = 'hymenoptera_data/val/'
-    train_size, train_loader = data_loader(train_dir, 4, train_transform)
-    val_size, val_loader = data_loader(val_dir, 4, val_transform)
-
+    # ----- 3 ----- #   
     #nn_model = nn_classifier.MLP([150528, 250, 2])
     #nn_model = nn_classifier.CNN([3, 6, 16], [16 * 53 * 53, 120, 84, 2], conv_ks=5, pool_ks=2)
     #nn_model = nn_classifier.existing_model('resnet18', 2)
-    nn_model = existing_model('resnet18', 2)
+    nn_model = existing('inception_v3', 5)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(nn_model.parameters(), lr=0.001, momentum=0.9)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-    nn_model = new_train(
+    nn_model = train(
             nn_model, 
             train_loader, val_loader, 
             train_size, val_size, 
             criterion, 
             optimizer,
             exp_lr_scheduler, 
-            num_epochs=5,
+            num_epochs=1,
             )
+
+
+
+
+
+    
